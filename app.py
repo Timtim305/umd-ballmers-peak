1b358197c99ec755ec4b4b0cd74fd4241879efe8
twang126 <timtim305@gmail.com>
2017-08-21 20:06:58 -0400

    Created sqlLite database

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, g
 import sqlite3
 
+DATABASE = 'Teams.db'
 
 print 'Everything compiles'
 
@@ -8,9 +9,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
+    
+    cur = get_db().cursor()
     return "Welcome!"
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
-
